b0b7e8fccc5b7b6c5214d8cc860e8c93c85a2027
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-03-11 16:30:38 -0300

    feat: add solution to Minimum Common Value problem

---
+++ problems/minimum-common-value/solution.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func getCommon(nums1 []int, nums2 []int) int {
+    m := make(map[int]int)
+
+    for _, r := range nums1 {
+        m[r]++
+    }
+    for _, r := range nums2 {
+        _, ok := m[r]
+        if ok {
+            return r
+        }
+    }
+
+	return -1
+}
+
+func main() {
+	fmt.Println(getCommon([]int{1, 2, 3}, []int{2, 4}) == 2)
+	fmt.Println(getCommon([]int{1, 2, 3, 6}, []int{2, 3, 4, 5}) == 2)
+	fmt.Println(getCommon([]int{1000000000, 1000000000}, []int{1000000000}) == 1000000000)
+}
